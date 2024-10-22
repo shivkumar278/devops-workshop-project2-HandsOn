59153ea78b97c210e6cf1089668fbba1748d2054
shiv <shiv@example.com>
2024-10-22 17:20:12 +0530

    creating ec2 with security group

---
+++ Terraform-code/ec2-with-sg.tf
@@ -0,0 +1,47 @@
+provider "aws" {
+  region = "us-east-1"
+}
+
+resource "aws_instance" "demo-server1" {
+    ami = "ami-022e1a32d3f742bd8"
+    instance_type = "t2.micro"
+    key_name = "dpp"
+    security_groups = [ "demo-sg" ]
+
+    tags = {
+    Name = "demo-server1"
+
+  }
+}
+
+resource "aws_security_group" "demo-sg1" {
+  name        = "demo-sg"
+  description = "SSH Access"
+  
+  ingress {
+    description      = "Shh access"
+    from_port        = 22
+    to_port          = 22
+
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+    }
+
+  egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+
+  tags = {
+    Name = "ssh-prot"
+
+  }
+}
+
+
+
+  
+

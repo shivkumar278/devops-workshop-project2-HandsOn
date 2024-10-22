db20fac640750e73d0badef7a1162b075470261d
shiv <shiv@example.com>
2024-10-22 12:52:58 +0530

    change ami

--- Terraform-code/ec2-01.tf
+++ Terraform-code/ec2-01.tf
@@ -3,7 +3,7 @@ provider "aws" {
 }
 
 resource "aws_instance" "demo-server" {
-    ami = "ami-022e1a32d3f742bd8"
+    ami = "ami-005fc0f236362e99f"
     instance_type = "t2.micro"
     key_name = "dpp"
 }
